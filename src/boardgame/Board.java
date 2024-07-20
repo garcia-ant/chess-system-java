f108f313786430bbe35fb6bd62ec85afbc2f9b09
garcia-ant <garcantony19@gmail.com>
2024-07-20 13:59:46 +0100

    Exception

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,6 +7,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+    	if(rows < 1 || columns < 1) {
+    		throw new BoardException("Error creating board: there must br at least 1 row and 1  column");	
+    	}
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -14,10 +17,6 @@ public class Board {
 
     public int getRows() {
         return rows;
-    }
-
-    public void setRows(int rows) {
-        this.rows = rows;
     }
 
     public int getColumns() {
@@ -29,15 +28,38 @@ public class Board {
     }
 
     public Piece piece(int row, int column) {
+    	if(!PositionExists(row,column)) {
+    		throw new BoardException("Position not on the board");
+    	}
         return pieces[row][column];
     }
 
     public Piece piece(Position position) {
+    	if(!PositionExists(position)) {
+    		throw new BoardException("Position not on the board");
+    	}
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position) {
-        pieces[position.getRow()][position.getColumn()] = piece;
+    	if(thereISAPiece(position)) {
+    		throw new BoardException("The is already a piece on position " + position);
+    	}
+    	pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+    
+    public boolean PositionExists(int row, int column) {
+    	return row >= 0 && row < rows && column >= 0 && column < columns; 
+    }
+    
+    public boolean PositionExists(Position position) {
+    	return PositionExists(position.getRow(),position.getColumn());
+    }
+    public boolean thereISAPiece(Position position) {
+    	if(!PositionExists(position)) {
+    		throw new BoardException("Position not on the board");
+    	}
+    		return piece(position) != null;
     }
 }
