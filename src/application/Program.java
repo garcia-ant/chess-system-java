6b303c7b751b9b84b7f694f421bb902b68c0b6b6
garcia-ant <garcantony19@gmail.com>
2024-07-21 00:48:37 +0100

    clearing screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,39 @@
 package application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import chess.ChessException;
 import chess.ChessMath;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		ChessMath chessMath =new ChessMath();
-		
-		UI.printBoard(chessMath.getPieces());
-	
+
+		Scanner sc = new Scanner(System.in);
+		ChessMath chessMath = new ChessMath();
+		while (true) {
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMath.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				ChessPiece capturePiece = chessMath.performChessMove(source, target);
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+
+			}
+		}
 	}
 
 }
