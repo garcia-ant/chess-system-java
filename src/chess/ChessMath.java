bd501baa031132f66016eb713e8e744fcee88b5d
garcia-ant <garcantony19@gmail.com>
2024-07-22 17:23:05 +0100

    Queen

--- src/chess/ChessMath.java
+++ src/chess/ChessMath.java
@@ -11,6 +11,7 @@ import chess_pieces.Bishop;
 import chess_pieces.King;
 import chess_pieces.Knigth;
 import chess_pieces.Pawn;
+import chess_pieces.Queen;
 import chess_pieces.Rook;
 
 public class ChessMath {
@@ -210,6 +211,7 @@ public class ChessMath {
         placeNewPiece('a', 1, new Rook(board, Color.WHITE));
         placeNewPiece('b', 1, new Knigth(board, Color.WHITE));
         placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('d', 1, new Queen(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
         placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('g', 1, new Knigth(board, Color.WHITE));
@@ -225,6 +227,7 @@ public class ChessMath {
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
         placeNewPiece('b', 8, new Knigth(board, Color.BLACK));
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
         placeNewPiece('g', 8, new Knigth(board, Color.BLACK));
--- src/chess_pieces/Queen.java
+++ src/chess_pieces/Queen.java
@@ -5,9 +5,9 @@ import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class Bishop extends ChessPiece {
+public class Queen extends ChessPiece {
 
-    public Bishop(Board board, Color color) {
+    public Queen(Board board, Color color) {
         super(board, color);
     }
 
@@ -21,14 +21,44 @@ public class Bishop extends ChessPiece {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
         Position p = new Position(0, 0);
 
-        // Northwest (NW)
-        p.setValues(position.getRow() - 1, position.getColumn() - 1);
+        // Up
+        p.setValues(position.getRow() - 1, position.getColumn());
         while (getBoard().positionExists(p) && canMove(p)) {
             mat[p.getRow()][p.getColumn()] = true;
             if (getBoard().piece(p) != null && ((ChessPiece) getBoard().piece(p)).getColor() != getColor()) {
                 break;
             }
-            p.setValues(p.getRow() - 1, p.getColumn() - 1);
+            p.setRow(p.getRow() - 1);
+        }
+
+        // Down
+        p.setValues(position.getRow() + 1, position.getColumn());
+        while (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            if (getBoard().piece(p) != null && ((ChessPiece) getBoard().piece(p)).getColor() != getColor()) {
+                break;
+            }
+            p.setRow(p.getRow() + 1);
+        }
+
+        // Left
+        p.setValues(position.getRow(), position.getColumn() - 1);
+        while (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            if (getBoard().piece(p) != null && ((ChessPiece) getBoard().piece(p)).getColor() != getColor()) {
+                break;
+            }
+            p.setColumn(p.getColumn() - 1);
+        }
+
+        // Right
+        p.setValues(position.getRow(), position.getColumn() + 1);
+        while (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            if (getBoard().piece(p) != null && ((ChessPiece) getBoard().piece(p)).getColor() != getColor()) {
+                break;
+            }
+            p.setColumn(p.getColumn() + 1);
         }
 
         // Northeast (NE)
@@ -61,11 +91,21 @@ public class Bishop extends ChessPiece {
             p.setValues(p.getRow() + 1, p.getColumn() - 1);
         }
 
+        // Northwest (NW)
+        p.setValues(position.getRow() - 1, position.getColumn() - 1);
+        while (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            if (getBoard().piece(p) != null && ((ChessPiece) getBoard().piece(p)).getColor() != getColor()) {
+                break;
+            }
+            p.setValues(p.getRow() - 1, p.getColumn() - 1);
+        }
+
         return mat;
     }
 
     @Override
     public String toString() {
-        return "B";
+        return "Q";
     }
 }
